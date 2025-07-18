6142c09be9d7ec6396b7a136d6ffcbd6859a3acc
Taksh17 <153715622+Taksh17@users.noreply.github.com>
2025-07-19 02:08:29 +0530

    Add files via upload

---
+++ Diagonaltraversal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][3]={10,20,30,40,50,60,70,80,90};
+    int rowsize=3;
+    int colsize=3;
+    for(int i=0;i<rowsize;i++){
+        //for(int j=0;j<colsize;j++){
+          //  if(i==j){
+            cout<<arr[i][i]<<" ";
+            }
+        
+        cout<<endl;
+    }
