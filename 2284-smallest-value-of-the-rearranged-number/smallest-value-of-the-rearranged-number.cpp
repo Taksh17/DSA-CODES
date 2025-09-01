e9f1852c2b51f8f58b96ce3a0719c4e6d85cd787
Taksh17 <153715622+Taksh17@users.noreply.github.com>
2025-09-02 02:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.47%) - LeetSync

---
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long smallestNumber(long long num) {
+         if (num == 0) return 0;
+
+    string s = to_string(abs(num));
+
+    if (num > 0) {
+        sort(s.begin(), s.end());
+        if (s[0] == '0') {
+            for (int i = 1; i < s.size(); i++) {
+                if (s[i] != '0') {
+                    swap(s[0], s[i]);
+                    break;
+                }
+            }
+        }
+        return stoll(s);
+    } else {
+        sort(s.rbegin(), s.rend());
+        return -stoll(s);
+    }
+        return stoll(s);
+    }
+};
