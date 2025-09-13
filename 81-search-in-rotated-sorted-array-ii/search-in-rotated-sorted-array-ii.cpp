577d2131c72c7d78c3343dcd3211d8b97dd99b1d
Taksh17 <153715622+Taksh17@users.noreply.github.com>
2025-09-14 00:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (54.99%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int s=0;
+        int e=nums.size()-1;
+        int m=s+(e-s)/2;
+        while(s<=e){
+            if(nums[m]==target){
+                return true;
+            }
+            else if(nums[s]==nums[m] and nums[m]==nums[e]){
+                s++;
+                e--;
+                continue;
+            }
+            else if(nums[s]<=nums[m]){
+                if(nums[s]<=target and target<=nums[m]){
+                    e=m-1;
+                }
+                else{
+                    s=m+1;
+                }
+            }
+            else if(nums[m]<=nums[e]){
+                if(nums[m]<=target and target<=nums[e]){
+                    s=m+1;;
+                }
+                else{
+                e=m-1;
+                }
+            }
+            m=s+(e-s)/2;
         }
-       if(mp.find(target)!=mp.end()){
-        return true;
-       }
-       return false;
+        return false;
         
     }
 };
